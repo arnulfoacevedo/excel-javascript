db1e1d8c829f62f900e238dcf62145eb1e586df4
arnulfoacevedo <arnulfoacevedo215@gmail.com>
2017-12-30 09:57:19 +0800

    Refactor app.js to use refactored columns/data inputs

--- app.js
+++ app.js
@@ -1,55 +1,21 @@
-const Excel = require('exceljs')
+const { writeToCsv, readFromCsv } = require('./exceljs')
 
-// Write CSV Files
-const writeToCsv = (filename, sheetName) => {
-  const workbook = createAndFillWorkbook(sheetName)
-  workbook.csv.writeFile(filename)
-    .then( () => {
-      // Done
-      console.log('Done')
-    })
-}
+// Array of column names
+columns = [
+  { header: 'Id', key: 'id', width: 10 },
+  { header: 'Name', key: 'name', width: 32 },
+  { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
+]
 
-// Create a Workbook and a sheet then fill it with data
-const createAndFillWorkbook = (sheetName) => {
-  const workbook = new Excel.Workbook()
-  const sheet = workbook.addWorksheet(sheetName);
+// Array of data rows
+data = [
+  { id: 1, name: 'John Doe', dob: new Date(1970, 1, 1) },
+  { id: 2, name: 'Jane Doe', dob: new Date(1965, 1, 7) },
+  [3, 'Sam', new Date()],
+  [4, 'Bob', new Date()], // row by array
+  { id: 5, name: 'Barbara', dob: new Date() }
+]
 
-  // Create Column Names
-  sheet.columns = [
-    { header: 'Id', key: 'id', width: 10 },
-    { header: 'Name', key: 'name', width: 32 },
-    { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
-  ]
-  // Create Row Data
-  sheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
-  sheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
-  sheet.addRow([3, 'Sam', new Date()]);
-  // Create Row Data using Array
-  const rows = [
-    [4,'Bob',new Date()], // row by array
-    {id:5, name: 'Barbara', dob: new Date()}
-  ];
-  sheet.addRows(rows);
+writeToCsv('test.csv', 'My Sheet', columns, data)
 
-  return workbook
-}
-
-
-// Read CSV Files
-const readFromCSV = (filename) => {
-  const workbook = new Excel.Workbook()
-  workbook.csv.readFile(filename)
-    .then(worksheet => {
-      
-      // Iterate over each row
-      worksheet.eachRow( (row, rowNumber) => {
-        console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
-      })
-    })
-}
-
-
-writeToCsv('test.csv', 'My Sheet')
-
-readFromCSV('test.csv')
+readFromCsv('test.csv')
