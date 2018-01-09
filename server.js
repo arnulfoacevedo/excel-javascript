0d505ce55150e99b310d000d29a1a811b628ac57
arnulfoacevedo <arnulfoacevedo215@gmail.com>
2018-01-09 16:06:47 +1100

    Add /uploadExcel route

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
 
 const server = express()
 
 // Middleware Plugins
 server.use(bodyParser.json())
+server.use(fileUpload())
+server.use(express.static('public')) // Just for testing, use a static html
 
 // Routes
 server.use('/', [
-  // require('./routes/rainfall')
+  require('./routes/uploadExcel')
 ])
 
 // Start the server
